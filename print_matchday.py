a2cc132da14ef86bae7efc4f2eae9a5588d05452
mark on github <markongithub@users.noreply.github.com>
2023-11-19 13:57:17 -0500

    it almost works

---
+++ print_matchday.py
@@ -0,0 +1,13 @@
+import random
+from llama_slobber import get_matchday
+
+questions = get_matchday(98, 8, 'B_Galaxy')[2]
+
+max_answer_length = max([len(question['answer']) for question in questions])
+intended_length = max_answer_length + random.randint(10, 20)
+
+for question in questions:
+    print(f"{question['number']}. {question['text']}")
+    padded = f" {question['answer']}".rjust(intended_length + 1, "_")
+    print(f">!`{padded}`!<")
+    print()
