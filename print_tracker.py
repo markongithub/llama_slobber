52e18bc580ecacbeb403890a61c66559f67a9063
mark on github <markongithub@users.noreply.github.com>
2024-06-30 12:03:35 -0400

    clean up formatting

--- print_tracker.py
+++ print_tracker.py
@@ -18,12 +18,19 @@ for division in TRACKED:
         tracked_results[player] = results[player]
 
 players_by_question = [[], [], [], [], [], []]
+forfeiters = set()
 for player in tracked_results:
     answers = tracked_results[player]["answers"]
     for i in range(6):
         if answers[i] == "1":
             players_by_question[i].append(player)
+        if answers[i] == "F":
+            forfeiters.add(player)
+submitted_players = total_players - len(forfeiters)
 for i in range(6):
     print(
-        f"Q{i+1}: {len(players_by_question[i])}/{total_players} {players_by_question[i]}"
+        f"Q{i+1}: {len(players_by_question[i])}/{submitted_players} {sorted(players_by_question[i])}"
     )
+print(
+    f"Out of {submitted_players} tracked players, {len(players_by_question[0])} got Q1, {len(players_by_question[1])} Q2, {len(players_by_question[2])} Q3, {len(players_by_question[3])} Q4, {len(players_by_question[4])} Q5, and {len(players_by_question[5])} Q6."
+)
